f904427076cb2d830adda0d7570ba06f75614fc9
gojjh <newrjflau@naver.com>
2019-07-23 13:22:56 +0900

    aaaaab

--- onnotify.cpp
+++ onnotify.cpp
@@ -1,52 +1,41 @@
-#include <eosio/eosio.hpp>
-#include <eosio/asset.hpp>
+#include<eosio/eosio.hpp>
+#include<eosio/asset.hpp>
 
 using namespace eosio;
 
-    CONTRACT onnotify: public contract {
-    public:
-        using contract::contract;
+CONTRACT onnotify: public contract {
+public:
+using contract::contract;
 
-    ACTION dummy() {}
+ACTION dummy() {}
 
-    [[eosio::on_notify("eosio.token::transfer")]]
-    void ontransfer(name from, name to, asset quantity, std::string memo) {
-        if(from == get_self()) {
-            Receiver receiver(get_self(), get_self().value);
-            auto itr = receiver.find(to.value);
-        if(itr == receiver.end()) {
-            receiver.emplace(from, [&](auto& row) {
-                row.user = to;
-                row.balance = quantity;
-        });
-    } else {
-        receiver.modify(itr, from, [&](auto& row) {
-        row.balance += quantity;
-    });
-}
-    } else {
-        Sender sender(get_self(), get_self().value);
-            auto itr = sender.find(from.value);
-    if(itr == sender.end()) {
-        sender.emplace(to, [&](auto& row) {
-            row.user = from;
-            row.balance = quantity;
-    });
-    } else {
-        sender.modify(itr, to, [&](auto& row) {
-            row.balance += quantity;
-        });
-    }
+[[eosio::on_notify("eosio.token::transfer")]]
+void ontransfer(name from, name to, asset quantity, std::string memo) {
+if(from == get_self()) {
+Receive forReceiver(get_self(), get_self().value);
+forReceiver.emplace(get_self(), [&](auto& row) {
+row.mykey = forReceiver.available_primary_key();
+row.user = from;
+row.balance = quantity;
+});
+} else {
+Send forSender(get_self(), get_self().value);
+forSender.emplace(get_self(), [&](auto& row) {
+row.mykey = forSender.available_primary_key();
+row.user = to;
+row.balance = quantity;
+});
 }
 }
-    private:
-        TABLE trxlist {
-            name user;
-            asset balance;
+private:
+TABLE tradingbook_struct {
+uint64_t mykey;
+name user;
+asset balance;
 
-    uint64_t primary_key() const { return user.value; }
-    };
+uint64_t primary_key() const { return mykey; }
+};
 
-typedef multi_index<"eossender"_n, trxlist> Sender;
-typedef multi_index<"eosreceiver"_n, trxlist> Receiver;
+typedef multi_index<"senderbook"_n, tradingbook_struct> Send;
+typedef multi_index<"receiverbook"_n, tradingbook_struct> Receive;
 };
